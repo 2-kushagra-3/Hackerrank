615e71ad0570713e188eb12ccd86ae61676fa590
Kushagra verma <kushagraid@gmail.com>
2022-09-30 14:04:58 +0530

    function changed

--- TextWrap.py
+++ TextWrap.py
@@ -1,7 +1,5 @@
 def wrap(string, max_width):
-    return 
-string=str(input())
-max_width=int(input())
-#print (string)
-#print(max_width)
-print(textwrap.fill(string,max_width))
+    wrapper = textwrap.TextWrapper(width=max_width) 
+    dedented_text = textwrap.dedent(text=string) 
+    result = wrapper.fill(text=dedented_text) 
+    return result
